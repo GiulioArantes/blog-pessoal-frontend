a98258c47c539a4e7e4fdeedc0a64a27ee2ecff0
GiulioArantes <giulio.arantes@icloud.com>
2025-05-28 20:43:39 -0300

    Componentes CardPostagens e ListaPostagens - Estilização e Lógica

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,29 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens";
+
 function Home() {
   return (
     <>
       <div className="bg-indigo-900 flex justify-center">
         <div className="container grid grid-cols-2 text-white">
-          <div className="flex flex-col gap-4 items-center justify-center py-4">
+          <div
+            className="flex flex-col gap-4 items-center
+                                    justify-center py-4"
+          >
             <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
             <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
             <div className="flex justify-around gap-4">
-              <div className="rounded text-white border-white border-solid border-2 py-4 px-4">
+              <div
+                className="rounded text-white 
+                                            border-white border-solid
+                                            border-2 py-2 px-4"
+              >
                 Nova Postagem
               </div>
             </div>
           </div>
 
-          <div className="flex justify-center">
+          <div id="imagem" className="flex justify-center">
             <img
               src="https://i.imgur.com/VpwApCU.png"
               alt="Imagem da Página Home"
@@ -23,6 +32,7 @@ function Home() {
           </div>
         </div>
       </div>
+      <ListaPostagens />
     </>
   );
 }
