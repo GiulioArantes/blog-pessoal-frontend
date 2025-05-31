061264377c7335763afb8f2519358ac312c6d570
GiulioArantes <giulio.arantes@icloud.com>
2025-05-30 21:21:08 -0300

    refactor(part 2): correções básicas.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -18,9 +18,9 @@ function Footer() {
       <div className="flex justify-center bg-indigo-900 text-white">
         <div className="container flex flex-col items-center py-4">
           <p className="text-xl font-bold">
-            Blog Pessoal Generation | Copyright: {data}
+            Blog Pessoal do Giulio Arantes | Copyright: {data}
           </p>
-          <p className="text-lg">Acesse nossas redes sociais</p>
+          <p className="text-lg">Acesse minhas redes sociais</p>
           <div className="flex gap-2">
             <a
               href="https://www.linkedin.com/in/giulio-arantes/"
